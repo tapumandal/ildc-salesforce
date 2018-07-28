dd7b109e1a6181b1c98abcfd578d9c2ae612effb
Shohidur Rahman <sohidurr49@gmail.com>
2018-07-28 19:51:12 +0600

    added

--- app/Http/Controllers/ifa/PartiallyCompleted.php
+++ app/Http/Controllers/ifa/PartiallyCompleted.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\ifa;
 
 use App\Http\Controllers\Message\ActionMessage;
+use App\Model\IfaManagement\IfaRegistration;
+use App\Model\IfaManagement\FilterOption;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Validator;
@@ -12,10 +14,29 @@ use DB;
 class PartiallyCompleted extends Controller
 {
     public function viewPartiallyCompleted(){
+
+    	$getFilterOptionValue = FilterOption::get();
+
     	$getListValue = DB::table('tbl_ifa_registrations')
-    						->where('application_status',2)
-                            // ->orderBy('id_organization','DESC')
-                            ->paginate(25);
-    	return view('ifa.partialty_completed.partialtyCompleteList',compact('getListValue'));
+                            ->orderBy('ifa_reg_id','DESC')
+                            ->paginate(15);                            
+    	return view('ifa.partialty_completed.partialtyCompleteList',compact('getListValue','getFilterOptionValue'));
+    }
+
+    public function getIfaAllValue(Request $request){
+    	return json_encode(IfaRegistration::get());
+    }
+    public function getIfaFilterValue(Request $request){
+
+		$aaa = new IfaRegistration();
+	 	
+	 	$data = $aaa->whereDate('created_at','>=',date($request->formDateValues))
+	 			->whereDate('created_at','<=',date($request->toDateValues))
+	 			->where('application_status',$request->selectedOptionValues)
+	 			->orderBy('application_no',(!empty($request->sortbyValues) ? $request->sortbyValues : "ASC"))
+	 			->get();
+
+    	// return $data;
+    	return json_encode($data);
     }
 }
