8cb21dcfab44da655f61a78b742485ebb039aff2
kamruzzaman <kamruzzaman@bookhint.com>
2018-07-31 13:14:48 +0600

    bulk

--- resources/views/sales_agent/bulk_upload/viewBulkUpload.blade.php
+++ resources/views/sales_agent/bulk_upload/viewBulkUpload.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.dashboard')
-@section('page_heading','Bulk Upload')
+@section('page_heading','')
 @section('section')
 <div class="row">
 	<div class="col-sm-12">
@@ -17,11 +17,29 @@
 </div>
 <div class="col-sm-8 col-sm-offset-2">
 	<div class="panel panel-default">
+		<div class="panel-heading">
+			<h3>
+				IFA/Sales Agent Upload
+			</h3>
+		</div>
 		<div class="panel-body">
 			<form action="{{ route('sales_bulk_upload_action')}}" method="POST" role="form" enctype="multipart/form-data">
 				{{csrf_field()}}
-					<div class="form-group">
-						<label class="col-sm-3 control-label" for="bulkupload"> <span class="pull-right">Upload Lead :</span></label>
+					<div class="form-group row">
+						<label class="col-sm-3 control-label" for="bulkupload"> <span class="pull-right">Institution</span></label>
+						<div class="col-sm-4">
+							<select class="form-control selections" name="institution" id="institution">
+								<option value="0">Select Insititution</option>
+								<option value="1">Insititution 1</option>
+								<option value="2">Insititution 2</option>
+								<option value="3">Insititution 3</option>
+								<option value="4">Insititution 4</option>
+								<option value="5">Insititution 5</option>
+							</select>
+						</div>
+					</div>
+					<div class="form-group row">
+						<label class="col-sm-3 control-label" for="bulkupload"> <span class="pull-right">IFA/Sales Agent :</span></label>
 						<div class="col-sm-5">
 							<input type="file" name="bulk" class="form-control-file" id="bulkupload">
 						</div>
@@ -41,6 +59,43 @@
 					</label>
 					<div class="col-sm-6">
 						<a href="{{ asset('excel/ifa.xlsx')}}" class="btn btn-default">Download excel <i class="fa fa-download"></i></a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<div class="col-sm-8 col-sm-offset-2">
+	<div class="panel panel-default">
+		<div class="panel-heading">
+			<h3>
+				Lead Upload
+			</h3>
+		</div>
+		<div class="panel-body">
+			<form action="{{ route('lead_bulk_upload_action')}}" method="POST" role="form" enctype="multipart/form-data">
+				{{csrf_field()}}
+					<div class="form-group row">
+						<label class="col-sm-3 control-label" for="bulkupload"> <span class="pull-right">Lead :</span></label>
+						<div class="col-sm-5">
+							<input type="file" name="bulk" class="form-control-file" id="bulkupload">
+						</div>
+	                    <div class="col-sm-4">
+	                        <button type="submit" class="btn btn-primary" style="margin-right: 15px;">
+	                        	<span class="pull-left">
+	                        		Upload
+	                        	</span>
+	                        </button>
+	                    </div>
+	            	</div>
+			</form>
+			<div class="col-sm-12">
+				<div class="form-group">
+					<label class="col-sm-4">
+					 	<span class="pull-right" style="padding-top: 8px">Prefered File Format :</span>
+					</label>
+					<div class="col-sm-6">
+						<a href="{{ asset('excel/lead.xlsx')}}" class="btn btn-default">Download excel <i class="fa fa-download"></i></a>
 					</div>
 				</div>
 			</div>
