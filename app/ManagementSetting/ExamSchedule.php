e448282d6bd175ec1f22dbae93f5b674dbd16b07
Nabodip Sutradhar <nabodip064@gmail.com>
2018-08-02 04:42:00 +0600

    modified

--- app/ManagementSetting/ExamSchedule.php
+++ app/ManagementSetting/ExamSchedule.php
@@ -3,8 +3,12 @@
 namespace App\ManagementSetting;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Model\ManagementSetting\ExamName;
 
 class ExamSchedule extends Model
 {
-    //
+
+    public function examName(){
+        return $this->hasOne(ExamName::class, 'id_exam_name', 'exam_name_id');
+    }
 }
